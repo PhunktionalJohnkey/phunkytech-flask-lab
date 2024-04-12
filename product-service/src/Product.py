d2f416f01cd45c378a1a0f4258a92a7965998620
Olaleye Alex-Oni <olaleyealex-oni@olaleyes-mbp.home>
2024-04-12 12:16:00 +0100

    adds logging feature to app

--- product-service/src/Product.py
+++ product-service/src/Product.py
@@ -1,7 +1,10 @@
 # This is the Product class
 
+import logging
 # Imports db to gain access to SQLAlchemy
 from db import db
+
+log = logging.getLogger(__name__)
 
 # Creates a new class named Product that extends db.Model
 class Product(db.Model):
@@ -19,17 +22,21 @@ class Product(db.Model):
     # Defines persistence and query methods
     @classmethod
     def find_by_id(cls, _id):
+        log.debug(f'Find product by id: {_id}')
         return cls.query.get(_id)
 
     @classmethod
     def find_all(cls):
+        log.debug('Query for all products')
         return cls.query.all()
 
     def save_to_db(self):
+        log.debug(f'Save Product to database: id= {self.id}, name={self.name}')
         db.session.add(self)
         db.session.commit()
 
     def delete_from_db(self):
+        log.debug(f'Delete Product from database: id= {self.id}, name={self.name}')
         db.session.delete(self)
         db.session.commit()
 
--- product-service/src/app.py
+++ product-service/src/app.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
 from flask import Flask, jsonify, request
+import logging.config
+from sqlalchemy import exc
 from db import db
 from Product import Product
 
-products = [
-    {'id': 1, 'name': 'Product 1'},
-    {'id': 2, 'name': 'Product 2'}
-]
+# Configure the logging package from the logging ini file
+logging.config.fileConfig("logging.ini", disable_existing_loggers=False)
+
+# Get a logger for our module
+log = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@db/products'
@@ -17,17 +20,27 @@ db.init_app(app)
 # curl -v http://localhost:5000/products
 @app.route('/products')
 def get_products():
-    products = [product.json for product in Product.find_all()]
-    return jsonify(products)
-
+    log.debug('GET /products')
+    try:
+        products = [product.json for product in Product.find_all()]
+        return jsonify(products)
+    except exc.SQLAlchemyError:
+        log.exception('An exception occurred while retrieving all products')
+        return 'An exception occurred while retrieving all products', 500
 
 # curl -v http://localhost:5000/product/1
 @app.route('/product/<int:id>')
 def get_product(id):
-    product = Product.find_by_id(id)
-    if product:
-        return jsonify(product.json)
-    return f'Product with id {id} not found', 404
+    log.debug(f'GET /product/{id}')
+    try:
+        product = Product.find_by_id(id)
+        if product:
+            return jsonify(product.json)
+        log.warning(f'GET /product/{id}: Product not found')
+        return f'Product with id {id} not found', 404
+    except exc.SQLAlcehmyError:
+        log.exception(f'An exception occurred while retrieving product {id}')
+        return f'An exception occurred while retrieving product {id}', 500
 
 
 # curl --header "Content-Type: application/json" --request POST --data '{"name": "Product 3"}' -v http://localhost:5000/product
@@ -37,47 +50,64 @@ def post_product():
 
     # Retrieve the product from the request body
     request_product = request.json
+    log.debug(f'POST /products with product: {request_product}')
 
     # Create a newe Product
     product = Product(None, request_product['name'])
 
-    # Save the product to the database
-    product.save_to_db()    
+    try:
+        # Save the product to the database
+        product.save_to_db()    
 
-    # Return the jsonified Product
-    return jsonify(product.json), 201
+        # Return the jsonified Product
+        return jsonify(product.json), 201
+    except exc.SQLALchemyError:
+        log.exception(f'An exception occurred while creating product with name: {product.name}')
+        return f'An exception occurred while creating product with name: {product.name}', 500
 
 
 # curl --header "Content-Type: application/json" --request PUT --data '{"name": "Updated Product 2"}' -v http://localhost:5000/product/2
 @app.route('/product/<int:id>', methods=['PUT'])
 def put_product(id):
- 
-    existing_product = Product.find_by_id(id)
+    log.debug(f'PUT /product/{id}')
+    try:
+        existing_product = Product.find_by_id(id)
 
-    if existing_product:
-        # Get the request payload
-        updated_product = request.json
+        if existing_product:
+            # Get the request payload
+            updated_product = request.json
 
-        existing_product.name = updated_product['name']
-        existing_product.save_to_db()
+            existing_product.name = updated_product['name']
+            existing_product.save_to_db()
 
-        return jsonify(existing_product.json), 200
+            return jsonify(existing_product.json), 200
+        
+        log.warning(f'PUT /product/{id}: Existing product not found')
+        return f'Product with id {id} not found', 404
+    
+    except exc.SQLAlchemyError:
+        log.exception(f'An exception occurred while updating product with name: {updated_product.name}')
+        return f'An exception occurred while updating product with name: {updated_product.name}', 500
 
-
-    return f'Product with id {id} not found', 404
 
 
 # curl --request DELETE -v http://localhost:5000/product/2
 @app.route('/product/<int:id>', methods=['DELETE'])
 def delete_product(id):
-    existing_product = Product.find_by_id(id)
-    if existing_product:
-        existing_product.delete_from_db()
-        return jsonify({
-            'message': f'Deleted product with id {id}'
-        }), 200
-
-    return f'Product with id {id} not found', 404
+    log.debug(f'DELETE /product/{id}')
+    try:
+        existing_product = Product.find_by_id(id)
+        if existing_product:
+            existing_product.delete_from_db()
+            return jsonify({
+                'message': f'Deleted product with id {id}'
+            }), 200
+        log.warning(f'DELETE /product/{id}: Existing product not found')
+        return f'Product with id {id} not found', 404
+    
+    except exc.SQLAlchemyError:
+        log.exception(f'An exception occurred while deleting the product with id: {id}')
+        return f'An exception occurred while deleting the product with id: {id}', 500
 
 
 if __name__ == '__main__':
